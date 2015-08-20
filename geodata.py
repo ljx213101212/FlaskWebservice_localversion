2f2f7fe9567db93b5501104aea68127c5e4e74ec
andyafter <andyafter@gmail.com>
2015-08-20 10:19:06 +0800

    json data for clinics

--- geodata.py
+++ geodata.py
@@ -1,1 +1,13 @@
 from database import *
+import json
+from pprint import pprint
+
+cdat = open('./data/Clinic.json','r')
+
+data = json.loads(cdat.read())
+print data.items()[0][1][1]  # this is one of the objects
+print data.items()[0][1][1][u'ADDRESS_1']
+
+
+
+cdat.close
